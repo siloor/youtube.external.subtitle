43a89c201139cbf49b15dce487b5f4387e19c8d2
Milan Magyar <milan.magyar@gmail.com>
2020-05-22 09:22:07 +0200

    Add tests for grantIframeApiScript

--- src/init.test.ts
+++ src/init.test.ts
@@ -2,6 +2,7 @@ import DIC from './dic';
 import init, {
   iframeApiScriptAdded,
   addIframeApiScript,
+  grantIframeApiScript,
   iframeApiLoaded,
   onIframeApiReady
 } from './init';
@@ -53,6 +54,55 @@ test('addIframeApiScript adds the iframe api script', () => {
   addIframeApiScript(document as Document);
 
   expect(insertHandler).toHaveBeenCalledWith(element, firstScript);
+});
+
+test('grantIframeApiScript adds the iframe api script if needed', () => {
+  const getDocument = (firstScript: HTMLScriptElement, element: HTMLElement, addIframeApi: boolean): Partial<Document> => {
+    return {
+      getElementsByTagName: (): HTMLCollectionOf<any> => {
+        const result = [ firstScript ];
+
+        if (addIframeApi) {
+          const iframeApiScript = {
+            src: 'https://www.youtube.com/iframe_api'
+          } as Partial<HTMLScriptElement>;
+
+          result.push(iframeApiScript as HTMLScriptElement);
+        }
+
+        return arrayToHTMLCollection(result);
+      },
+      createElement: (): HTMLElement => {
+        return element;
+      }
+    };
+  };
+
+  const element = {};
+  const firstScript = {
+    parentNode: {
+      insertBefore: jest.fn()
+    } as Partial<ParentNode & Node>
+  } as Partial<HTMLScriptElement>;
+
+  const document = getDocument(firstScript as HTMLScriptElement, element as HTMLElement, false);
+
+  grantIframeApiScript(document as Document);
+
+  expect(firstScript.parentNode.insertBefore).toHaveBeenCalledWith(element, firstScript);
+
+  const element2 = {};
+  const firstScript2 = {
+    parentNode: {
+      insertBefore: jest.fn()
+    } as Partial<ParentNode & Node>
+  } as Partial<HTMLScriptElement>;
+
+  const document2 = getDocument(firstScript2 as HTMLScriptElement, element2 as HTMLElement, true);
+
+  grantIframeApiScript(document2 as Document);
+
+  expect(firstScript2.parentNode.insertBefore).not.toHaveBeenCalled();
 });
 
 test('iframeApiLoaded returns the correct result', () => {
